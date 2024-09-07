915c3c29e1de8593208e86b7d6c587c5fa50c79d
DavidMTomas <143930697+DavidMTomas@users.noreply.github.com>
2024-09-07 22:12:09 +0200

    parte de queryslector

--- app.js
+++ app.js
@@ -31,14 +31,54 @@ const body = document.body;
 const body = document.body;
 
 // acceder al hijo
-console.log("Numero de elementos hijos "+ body.childNodes.length)
+console.log("1---Numero de elementos hijos "+ body.childNodes.length)
 
 // nombres de cada hijo
 for (let actual of body.childNodes){
     console.log("Nombres "+ actual.nodeName +" : "+ actual.nodeValue)
 }
 
-let wrapper = document.body.hasChildNodes()
-console.log(wrapper)
+let cuerpo = document.body
+console.log("2---Tiene hijos " + cuerpo.hasChildNodes())
+
+console.log("3---Hijos de cuerpo: "+document.body.children.length)
+
+console.log("4---Hijos de "+cuerpo.nodeName)
+for (let actual of cuerpo.children){
+    console.log(actual.nodeName)
+    console.log("  atributos "+ actual.getAttributeNames().toString())
+}
+
+console.log("5---Atributos hijos de cuerpo" + document.body.getAttributeNames().toString())
+
+
+
+console.log("6---Primer hijo "+cuerpo.firstElementChild)
+
+console.log("7--id primer hijo: "+cuerpo.firstElementChild.getAttribute("id"))
+
+console.log("8---Hijos de primer hijo "+cuerpo.firstElementChild.children.length)
+
+console.log("9--Hermano "+ cuerpo.firstElementChild.nextElementSibling)
+
+const primerHijoCuerpo = document.getElementById("wrapper")
+
+const primerHijoWrapper = primerHijoCuerpo.children[0]
+
+const segundoHijoWrapper = primerHijoCuerpo.children[1]
+
+const primerHijoSegundoHijoWrapper = segundoHijoWrapper.children[0]
+
+
+
+primerHijoSegundoHijoWrapper.addEventListener("click", function(){
+
+    primerHijoWrapper.textContent = "Estoy siendo modificado por le boton"
+
+});
+
+
+
+
 
 
